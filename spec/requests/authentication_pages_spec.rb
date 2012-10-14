75baf5a4347be4ede9102380f03f273de3f7660d
Ali Eslami <indigoae@hotmail.com>
2012-10-13 20:18:49 -0700

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,6 +4,69 @@ describe "Authentication" do
 
   subject { page }
 
+  describe "Authorization" do
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
+      end
+
+
+    end
+
+    describe "for non-signed in user" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "visiting the edit page" do
+        before { visit edit_user_path(user) }
+
+        it { should have_selector('h1',    text: 'Sign in') }
+        it { should have_content ('Please sign in.') }
+
+        describe "after signing in" do
+          before { sign_in user }
+          it { should have_selector('title', text: "Edit user")}
+        end
+      end
+
+      describe "submitting to the update action" do
+        before { put user_path(user) }
+        specify { response.should redirect_to(signin_path) }
+      end
+
+      describe "visiting the user index" do
+        before { visit users_path }
+        it { should have_selector('title', text: 'Sign in') }
+        it { should have_content ('Please sign in.') }
+      end
+    end
+
+    describe "for wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+
+      before { sign_in user }
+
+      describe "visit different persons edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_selector('title', text: full_title('Edit user')) }
+      end
+
+      describe "submitting to another users update" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+
+
+    end
+  end
+
   describe "signin page" do
     before { visit signin_path }
 
@@ -32,17 +95,16 @@ describe "Authentication" do
 
     end
 
-    describe "sign in success" do
+    describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
 
-      before do
-        fill_in "Email", with: user.email
-        fill_in "Password", with: user.password
-        click_button submit
-      end
+      before { sign_in user }
 
       it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users',    href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in') }
 
