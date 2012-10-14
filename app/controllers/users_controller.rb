5af85b2919a488bb8a3bdb88e95734a4a12e5232
Ali Eslami <indigoae@hotmail.com>
2012-10-14 02:17:20 -0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 15)
   end
 
   def new
@@ -60,25 +61,18 @@ class UsersController < ApplicationController
 
   private
 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
-  end
-
   def not_signed_in
     unless !signed_in?
       redirect_to root_path
     end
   end
 
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
-  end
-
-  def admin_user
-    redirect_to(root_path) unless current_user.admin?
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,6 +7,9 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_selector('h1',    text: user.name) }
@@ -16,6 +19,13 @@ describe "User pages" do
       it { should_not have_link('Settings') }
       it { should_not have_link('Profile') }
     end
+
+    describe "It should have two microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+
   end
 
   describe "signup page" do
